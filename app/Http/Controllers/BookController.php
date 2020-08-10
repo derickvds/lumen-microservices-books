ef05ca2545138c106cf95e4b1fbdfdf7104aeb42
Derick van der Sandt <derickvds@gmail.com>
2020-08-10 22:59:34 +0200

    Added books migration, model, seeder and controller

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class BookController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Return full list of books
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Create a new book
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Obtain and show book
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function show($book)
+    {
+
+    }
+
+    /**
+     * Update an existing book
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function update(Request $request, $book)
+    {
+
+    }
+
+    /**
+     * Delete an existing book
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function destroy($book)
+    {
+
+    }
+
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->post('/books', 'BookController@store');
+$router->get('/books/{book}', 'BookController@show');
+$router->put('/books/{book}', 'BookController@update');
+$router->patch('/books/{book}', 'BookController@update');
+$router->delete('/books/{book}', 'BookController@destroy');
